5f3d463b02f4944ec7fdc6fd4bc808b510a8fc58
Parag Naikade <paragnaikade@gmail.com>
2018-01-25 10:42:17 +0530

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    /// <summary>
+    /// C# delegates are similar to pointers to functions, in C or C++. 
+    /// With the help of deletgates, methods can be passed as a parameter to another methods.
+    /// A delegate is a reference type variable that holds the reference to a method. 
+    /// The reference can be changed at runtime.
+    /// </summary>
+    class Program
+    {
+        private static int result;
+
+        static void Main(string[] args)
+        {
+            DoOperations add = new DoOperations(Add);
+            Console.WriteLine("Addition: " + add(2, 3));
+
+            DoOperations subtract = Subtract;
+            Console.WriteLine("Subtraction: " + subtract(2, 3));
+
+            DoOperations multiply = Multiply;
+            Console.WriteLine("Multiplication: " + multiply(2, 3));
+
+            // This is multicast delegate. When delegate is invoked, seris of methods will be invoked.
+            // In this case, first Add method and then Multiply method will be invoked.
+            add += multiply;
+
+            Console.WriteLine("Addition: " + CallBackMethod(add, 2, 3));
+            Console.WriteLine("Subtraction: " + CallBackMethod(subtract, 2, 3));
+            Console.WriteLine("Multiplication: " + CallBackMethod(multiply, 2, 3));
+
+
+            Console.ReadKey();
+        }
+
+        // Declaration of deletgate
+        delegate int DoOperations(int a, int b);
+
+        static int CallBackMethod(DoOperations doOperations, int a, int b)
+        {
+            result = doOperations(a, b);
+
+            return result;
+        }
+
+        static int Add(int a, int b)
+        {
+            result = a + b;
+
+            return result;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            result = result * a * b;
+
+            return result;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ MyProjects/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MyProjects
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Yield keyword is used when we have a huge collection and we don't need
+    /// all items at once. So in this case, we get items one by one and we can do processing on it.
+    /// If we want to break this loop, use yield break.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var data in GetData())
+            {
+                Console.WriteLine(data);
+            }
+
+            Console.Read();
+        }
+
+        private static IEnumerable<int> GetData()
+        {
+            for (var i = 0; i < 1000000; i++)
+            {
+                if (i == 5)
+                {
+                    // This breaks the loop.
+                    yield break;
+                }
+
+                //// returns control to caller.
+
+                yield return i;
+            }
+        }
+    }
+}
