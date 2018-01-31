f9a665d6eba12c8693a2651ed78b0e7841e2105c
Parag Naikade <paragnaikade@gmail.com>
2018-01-31 17:55:25 +0530

    Changes in abstract class

--- Abstract/Program.cs
+++ Abstract/Program.cs
@@ -21,6 +21,11 @@ because it would be pointless.
              But because static class cannot be derived there is no way other class will implement these gaps.
         Static class cannot be marked sealed because it is made sealed by compiler by default.
 If abstract class is derived from sealed, then we cannot derive from that abstarct class, then there is no point of making it abstract.
+3. Abstract methods must be public.
+4. Abstract methods cannot have implementation. 
+    They are just defined in abstract class and need to be implemented in derived classes.
+5. All abstract methods must be overridden by derived classes using override keyword.
+6. We can have abstract properties as well.
 ");
 
             //// This is not allowed.
@@ -32,15 +37,29 @@ If abstract class is derived from sealed, then we cannot derive from that abstar
 
     abstract class Car
     {
-        
+        public abstract string Name { get; set; }
+
+        abstract public void Start();
+
+        abstract public void Stop();
+
+        void Wash()
+        {
+            
+        }
+
     }
 
-    public interface ICar
+    class Bmw : Car
     {
-        
-    }
+        public override string Name { get; set; }
 
-    class Bmw
-    {
+        public override void Start()
+        {
+        }
+
+        public override void Stop()
+        {
+        }
     }
 }
