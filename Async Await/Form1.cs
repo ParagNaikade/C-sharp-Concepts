5d46fd179b19e6fd5683830852e70f05b0cfcb0d
Parag Naikade <paragnaikade@gmail.com>
2018-02-26 15:14:24 +0530

    Added project async/await

---
+++ Async Await/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async_Await
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Main thread1: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            // This will cause a deadlock.
+            // Accessing the task.Result property will block the Main Thread if task is not complete.
+            // https://olitee.com/2015/01/c-async-await-common-deadlock-scenario/
+            textBox1.Text = DoSomething().Result;
+
+            Console.WriteLine("Main thread: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private async Task<string> DoSomething()
+        {
+            Console.WriteLine("DoSomething1: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine("DoSomething2: {0}", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            return "OK";
+        }
+    }
+}
